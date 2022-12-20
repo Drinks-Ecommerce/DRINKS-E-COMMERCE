42a2f3bdd5dd3b20f1fd371d1e032e068b4ac7ca
Octasanchezz <101231228+Octasanchezz@users.noreply.github.com>
2022-12-20 15:45:18 -0300

    Routes: Update, Delete, getId

--- api/src/controllers/products.controller.js
+++ api/src/controllers/products.controller.js
@@ -26,3 +26,52 @@ export const createProduct = async (req, res) => {
         res.status(400).json({ msg: error.msg })
     }
 }
+
+export const updateProduct = async (req, res) => {
+
+    try {
+        const { id } = req.params;
+        const { name, amount, worth } = req.body
+
+        const product = await Product.findByPk(id);
+        product.name = name;
+        product.amount = amount;
+        product.worth = worth;
+
+        await product.save();
+        res.json(product)
+
+    } catch (error) {
+        return res.status(400).json({ msg: error.msg })
+    }
+}
+
+export const deleteProduct = async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        await Product.destroy({
+            where: {
+                id,
+            }
+        })
+        res.json({ msg: "Deleted product" })
+    } catch (error) {
+        return res.status(400).json({ msg: error.msg })
+    }
+}
+
+export const getProduct = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const product = await Product.findOne({
+            where: {
+                id
+            }
+        })
+        if (!product) return res.status(404).json({ msg: "The product does not exist" })
+        res.json(product)
+    } catch (error) {
+        return res.status(400).json({ msg: error.msg })
+    }
+}
--- api/src/routes/products.routes.js
+++ api/src/routes/products.routes.js
@@ -1,12 +1,12 @@
 import { Router } from "express";
-import { getProducts, createProduct } from "../controllers/products.controller.js";
+import { getProducts, createProduct, updateProduct, deleteProduct, getProduct } from "../controllers/products.controller.js";
 
 const router = Router()
 
 router.get('/products', getProducts)
 router.post('/products', createProduct)
-router.put('/products/:id')
-router.delete('/products/:id')
-router.get('/products/:id')
+router.put('/products/:id', updateProduct)
+router.delete('/products/:id', deleteProduct)
+router.get('/products/:id', getProduct)
 
 export default router;
