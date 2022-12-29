632634d0cdee8c10999d4c1e82eaa24ed27270ae
josearturomendoza <91449803+josearturomendoza@users.noreply.github.com>
2022-12-29 00:32:46 -0300

    cambios home jsx y css

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -11,15 +11,41 @@ export default function Home(){
     return(
         <div className="contenedor_general">
 
-            {/* CONTENIDO PARA EL NAVBAR */}
+            {/* CONTENIDO PARA EL ENCABEZADO */}
+
+            <div className="conte_encabezado">
+
+                <div>
+                    LOGO
+                </div>
+
+                <div className="logo">
+                    SEARCHBAR
+                </div>
+
+                <div>
+                    LOGIN
+                </div>
+
+                <div>
+                    LOGOCARRITO
+                </div>
+
+
+            </div>
+
+        {/* CONTENIDO PARA EL NAVBAR */}
 
             <div className="nav">
                 <NavBar/>
             </div>
 
-            <Link to={"/cards"}>
-                <h1>Vinos</h1>
-            </Link>
+        
+            <div className="vinos">
+                <Link to={"/cards"}>
+                    <h1>Vinos</h1>
+                </Link>
+            </div>
 
             {/* CONTENIDO PARA EL FOOTER */}
 
