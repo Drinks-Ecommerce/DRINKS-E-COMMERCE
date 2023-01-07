5e409bcfad77969bee60896de82231c62bc7c8f7
josearturomendoza <91449803+josearturomendoza@users.noreply.github.com>
2023-01-07 19:41:45 -0300

    modifcacion navbar

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -4,15 +4,16 @@ export default function NavBar({
     handlefilterbyvinos,
     handelfilterbycerveza,
     handlewhiskys,
-    handlerones
-}
+    handlerones,
+    handleclick
+ }
 
 ){
 
     //className="filter_vinos"
 
     return(
-        <div class="grid grid-cols-4 gap-4 bg-black py-3 pb-3 mt-20 fixed top-0 left-0 right-0 drop-shadow-lg flex">
+        <div class="grid grid-cols-5 gap-4 bg-black py-3 pb-3 mt-20 fixed top-0 left-0 right-0 drop-shadow-lg flex">
            
              <div>
                 <select class="bg-wh-100 rounded-[12px] text-lg text-red-700 cursor-pointer text-center "  onChange={(e)=>handlefilterbyvinos(e)}>
@@ -40,6 +41,10 @@ export default function NavBar({
                     <option disabled selected>RONES</option>
                     <option value='rones'>TODOS LOS RONES</option>
                 </select>
+            </div>
+
+            <div>
+            <button type="button" className="hidden px-6 py-2 font-semibold rounded lg:block bg-teal-400 text-gray-600" onClick={(e)=>{handleclick(e)}}>Cargar</button>
             </div>
 
 
