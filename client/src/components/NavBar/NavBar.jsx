3efc0bcde8b5423f2ce3c8c1e80200e433d928f4
LucianoVilla95 <88258891+LucianoVilla95@users.noreply.github.com>
2023-01-07 18:58:34 -0300

    Filtros

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,23 +1,22 @@
 import React from "react";
+import { useDispatch } from 'react-redux';
+import { filterByTypes } from '../../action/index.js';
 
-export default function NavBar({
-    handlefilterbyvinos,
-    handelfilterbycerveza,
-    handlewhiskys,
-    handlerones
+export default function NavBar(){
+
+const dispatch = useDispatch();
+
+function handleFilterByVino(e) {
+    dispatch(filterByTypes(e.target.value));
 }
-
-){
-
-    //className="filter_vinos"
 
     return(
         <div class="grid grid-cols-4 gap-4 bg-black py-3 pb-3 mt-20 fixed top-0 left-0 right-0 drop-shadow-lg flex">
            
              <div>
-                <select class="bg-wh-100 rounded-[12px] text-lg text-red-700 cursor-pointer text-center "  onChange={(e)=>handlefilterbyvinos(e)}>
+                <select class="bg-wh-100 rounded-[12px] text-lg text-red-700 cursor-pointer text-center "  onChange={(e)=>handleFilterByVino(e)}>
                     <option disabled selected>VINOS</option>
-                    <option value='vinos'>TODOS LOS VINOS</option>
+                    <option value='vino'>TODOS LOS VINOS</option>
                 </select>
             </div>
 
