a1113144c518285b2890195ec9f42d96701b6f62
Agustina <aguavalos@hotmail.com>
2023-01-08 23:21:22 -0300

    Update byBrand.js

--- api/src/routes/filters/byBrand.js
+++ api/src/routes/filters/byBrand.js
@@ -1,53 +1,51 @@
 const { Router } = require ("express");
-const { Products, Type} =  require( "../../db");
-const { Op } = require("sequelize");
-const {getProducts} = require("../../controllers/productsController");
+const { getProduct, getProducts } = require("../../controllers/productsController");
+const { Products, Type } =  require( "../../db");
 
 const router = Router();
 
+
 router.get('/', async(req,res)=>{
-    try {
+    try{
         const { brand } = req.query;
-        const allProducts = await getProducts()
-    
         const productByBrand = await Products.findAll({
-            where: {
-                brand: { [Op.iLike]: `%${brand}%` }
+             where: {
+                brand: brand,
             },
             include: {
                 model: Type,
                 attributes: ["name"],
                 through: {
-                attributes: [],
-                },
-            },
-        })
-    
-        if(!productByBrand.length){
-        res.send({ msg: "No se encontraron productos con esa marca" });
-        }else{ 
-            const response = await productByBrand.map(e => {
-                return {
-                    id: e.id,
-                    name: e.name,
-                    stock:e.stock,
-                    price:e.price,
-                    description:e.description,
-                    brand:e.brand,
-                    discount:e.discount,
-                    origin:e.origin,
-                    alcohol:e.alcohol,
-                    img:e.img,
-                    comments:e.comments,
-                    calification:e.calification,
-                    type: e.types.map((e)=> e.name)
-    
+                    attributes: [],
                 }
-            })
-            res.send(response)
-        }
-    } catch (error) {
-        console.log(error);
+            }
+        })
+        if(!productByBrand.length){
+            res.send("categoria invalida")
+        }else{
+        const response = await productByBrand.map(e => {
+            return {
+                id: e.id,
+                name: e.name,
+                stock:e.stock,
+                price:e.price,
+                description:e.description,
+                brand:e.brand,
+                discount:e.discount,
+                origin:e.origin,
+                alcohol:e.alcohol,
+                img:e.img,
+                comments:e.comments,
+                calification:e.calification,
+                type: e.types.map((e)=> e.name)
+
+            }
+        })
+        res.send(response)
+    }
+      
+    }catch(err){
+        console.log(err)
     }
 });
 
