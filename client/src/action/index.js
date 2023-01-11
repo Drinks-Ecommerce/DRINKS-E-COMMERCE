eddeda0fdc52620ee729879a4720d8f7e860171c
josearturomendoza <91449803+josearturomendoza@users.noreply.github.com>
2023-01-11 16:59:38 -0300

    prueba

--- client/src/action/index.js
+++ client/src/action/index.js
@@ -74,6 +74,36 @@ export function getDetail(id) {
     };
 };
 
+// export function deleteProduct(id) {
+//     return async function(dispacth) {
+//         const json = await axios.delete(`http://localhost:3000/products/${id}`);
+//         return dispatch({
+//             type: 'DELETE_PRODUCT',
+//             payload: json.data
+//         });
+//     };
+// };
+
+// export function updateProduct(id) {
+//     return async function(dispatch) {
+//         const json = await axios.update(`http://localhost:3000/products/${id}`);
+//         return dispatch({
+//             type: 'UPDATE_PRODUCT',
+//             payload: json.data
+//         });
+//     };
+// };
+
+// export function deleteType(id) {
+//     return async function(dispatch) {
+//         const json = await axios.delete(`http://localhost:3000/types/delete/${id}`);
+//         return dispatch({
+//             type: 'DELETE_TYPE',
+//             payload: json.data
+//         })
+//     }
+// };
+
 export function filterByPriceOrder(payload) {
     return async function(dispatch) {
         const json = await axios(`http://localhost:3000/products/byprice/${payload}`);
@@ -93,3 +123,4 @@ export function filterByTypes(payload) {
         });
     };
 };
+
