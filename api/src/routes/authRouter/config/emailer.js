6ac0a2f96c9fde41080f265dd78fcae633a4ccf1
Octasanchezz <101231228+Octasanchezz@users.noreply.github.com>
2023-01-12 21:48:36 -0300

    Update .env

--- api/src/routes/authRouter/config/emailer.js
+++ api/src/routes/authRouter/config/emailer.js
@@ -1,29 +1,30 @@
 const nodemailer = require("nodemailer");
 
 const {
-    PASSEMAILER,MAIL
-  } = process.env;
-  
+    PASSEMAILER, MAIL
+} = process.env;
 
-    const enviarMail = async(mail,token) =>{
-        const config = {
+
+const enviarMail = async (mail, token) => {
+    const config = {
         host: "smtp.gmail.com",
         port: 587,
         auth: {
             user: MAIL,
-            pass:PASSEMAILER,
-        },}
+            pass: PASSEMAILER,
+        },
+    }
 
-        const mensaje ={
-            from :MAIL,
-            to:mail,
-            subject:"correo de prueba",
-            html:"holi estoy probando " + token
-        }
-        const transport = nodemailer.createTransport(config);
-        const info = await transport.sendMail(mensaje)
+    const mensaje = {
+        from: MAIL,
+        to: mail,
+        subject: "correo de prueba",
+        html: "holi estoy probando " + token
+    }
+    const transport = nodemailer.createTransport(config);
+    const info = await transport.sendMail(mensaje)
 
-        console.log(info)
-        
-  
-module.exports = {createTrans};
+    console.log(info)
+}
+
+module.exports = { enviarMail };
