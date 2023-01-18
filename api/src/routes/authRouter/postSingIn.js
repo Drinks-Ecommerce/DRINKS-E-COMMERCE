8e0bf8e8cf87a61e3a6d9450208d9d2245cf45d4
AgusCorcico <corcicoagustin@gmail.com>
2023-01-18 09:01:35 -0300

    Update postSingIn.js

--- api/src/routes/authRouter/postSingIn.js
+++ api/src/routes/authRouter/postSingIn.js
@@ -3,7 +3,7 @@ const { Router } = require("express")
 const bcrypt = require("bcryptjs");
 const { comparePassword } = require("./helpers/handleBcrypt")
 const jwt = require("jsonwebtoken")
-const { User } = require('../../db');
+const { User, Role } = require('../../db');
 const config = require("./config/auth");
 
 const router = Router();
@@ -12,6 +12,7 @@ router.post("/", async (req, res) => {
     const { email, password } = req.body;
 
     const userFound = await User.findOne({
+        include:[{model: Role}],
         where: { email }
     })
     if (!userFound) return res.status(404).json({ msg: "User not found" })
