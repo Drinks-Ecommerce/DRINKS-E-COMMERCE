e02373f62d85bf3f68bb7ef84f73861dc66e0fc6
LucianoVilla95 <88258891+LucianoVilla95@users.noreply.github.com>
2023-01-20 16:26:52 -0300

    Solucion de conflicto

--- client/src/reducer/index.js
+++ client/src/reducer/index.js
@@ -8,6 +8,7 @@ const initialState = {
     allOrigins: [],
     userRol: [],
     userr:[],
+    wishlist: [],
 
     // product: [], ruta echa para que se la use en el componente del admin para que pueda borrar y modificar el producto.
     user: {},
@@ -255,6 +256,11 @@ function rootReducer(state = initialState, action){
                 ...state
             }
 
+        case 'GET_WISHLIST':
+            return {
+                ...state,
+                wishlist: action.payload
+            }
 
 //-------------------------------- CASE DEFAULT --------------------------------------
 
