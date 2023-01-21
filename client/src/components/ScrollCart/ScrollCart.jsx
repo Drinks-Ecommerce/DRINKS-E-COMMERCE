3a56cd6e092a2920c2d0d13caa0a6e9c06147f80
Victor Pestana <60305666+stringVictorinox@users.noreply.github.com>
2023-01-20 22:11:28 -0400

    cambios-inicio

--- client/src/components/ScrollCart/ScrollCart.jsx
+++ client/src/components/ScrollCart/ScrollCart.jsx
@@ -14,27 +14,29 @@ export default function ScrollCart({cart}){
     
     return(
 
-        <ul className="flex flex-col divide-y-2 divide-gray-700">
+        <ul className="flex flex-col divide-y-2 divide-gray-500">
             
             {
                 cart?.map((e) => {
 
                     return(
 
-		                <li className="flex flex-col py-6 sm:flex-row sm:justify-between">
+		                <li className="flex flex-col py-3 sm:flex-row sm:justify-between">
                           
-						    <div className="flex w-full space-x-2 sm:space-x-4">
+						    <div className="flex w-full space-x-2 mt-2 sm:space-x-4">
 							    <img className="flex-shrink-0 object-cover w-20 h-20 border-transparent rounded outline-none sm:w-24 sm:h-24 bg-gray-500"
 								    src={e.img} alt={e.img} />
 
 								<div className="flex flex-col justify-between w-full pb-4">
 					                <div className="flex justify-between w-full pb-2 space-x-2">
 						                <div className="space-y-1">
-							                <h3 className="text-lg font-semibold leading-snug sm:pr-8">{e.name}</h3>
-							                <p className="text-sm text-gray-500">BRAND</p>
+							                <h3 className="text-sm font-semibold leading-snug sm:pr-8">{e.name}</h3>
+											<p className='text-sm text-gray-500'>{e.brand}</p>
+							                <p className="text-sm text-gray-300">Cantidad: {e.quantity}</p>
 						                </div>
 						                <div className="text-right">
-							                 <p className="text-lg font-semibold pr-2">${e.price}</p>
+							                 <p className="text-lg font-semibold pr-2">${e.totalValue}</p>
+											<p className='text-xs text-gray-400 pr-2'>1 x ${e.priceProduct}</p>
 						                </div>
 					                </div>
 
@@ -49,7 +51,7 @@ export default function ScrollCart({cart}){
 							                </svg>
 
 							                <button onClick={() => handleClick(e.id)}>
-                                                <h1>Remove</h1>
+                                                <span>Remove</span>
                                             </button>
 						                </button>
 
@@ -57,7 +59,6 @@ export default function ScrollCart({cart}){
 							                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="w-4 h-4 fill-current">
 								                <path d="M453.122,79.012a128,128,0,0,0-181.087.068l-15.511,15.7L241.142,79.114l-.1-.1a128,128,0,0,0-181.02,0l-6.91,6.91a128,128,0,0,0,0,181.019L235.485,449.314l20.595,21.578.491-.492.533.533L276.4,450.574,460.032,266.94a128.147,128.147,0,0,0,0-181.019ZM437.4,244.313,256.571,425.146,75.738,244.313a96,96,0,0,1,0-135.764l6.911-6.91a96,96,0,0,1,135.713-.051l38.093,38.787,38.274-38.736a96,96,0,0,1,135.765,0l6.91,6.909A96.11,96.11,0,0,1,437.4,244.313Z"></path>
 							                </svg>
-							                
                                             <span>Add to favorites</span>
 						                </button>
 					                </div>
