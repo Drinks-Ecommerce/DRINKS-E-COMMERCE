74f98d6ed9109f274da724957a3e2c8cfc1a08b8
Agustina <aguavalos@hotmail.com>
2023-01-23 14:21:55 -0300

    post para admin

--- api/src/models/Role.js
+++ api/src/models/Role.js
@@ -6,7 +6,6 @@ module.exports = (sequelize) => {
     sequelize.define('role', {
         name: {
             type: DataTypes.STRING,
-            allowNull: false,
             defaultValue:"user"
         }
 
