e33768012207057b4bde1f65069ebb1f41fcc840
Agustina <aguavalos@hotmail.com>
2023-01-24 21:04:38 -0300

    debug de botones de formularios

--- client/src/components/Register/Register.jsx
+++ client/src/components/Register/Register.jsx
@@ -92,23 +92,21 @@ export default function Registrar(){
     return (
 
         <div className="flex flex-col bg-gray-400 h-screen">
-
             <div>
                 <Encabezado />
             </div>
 
             <div className="mx-auto mt-4 mb-4 flex flex-col max-w-3xl w-4/5 p-6 rounded-md sm:p-6 bg-gray-900 text-gray-100 ">
-
 	            <div className="mb-4 text-center">
 		            <h1 className="my-3 text-4xl font-bold underline">Registrarse</h1>
 	            </div>
 
 	            <form  onSubmit={ onSubmit } className="space-y-5 ng-untouched ng-pristine ng-valid">
-
                     <div className='flex flex-row justify-between mx-8'>
 
-
                     <div className='flex flex-col'>
+
+
 
 				    <div className="space-y-1 mb-4">
 					        <label className="block mb-1 text-sm ml-1">Nombre</label>
@@ -117,6 +115,8 @@ export default function Registrar(){
                              />
                               <span>{errors.name && (<p className='error'>{errors.name}</p>)}</span>
 				    </div>
+
+
                     
                     <div className="space-y-1 mb-4">
 					    <label className="block mb-1 text-sm ml-1">Apellido</label>
@@ -124,7 +124,11 @@ export default function Registrar(){
                          text-gray-100"  value={input.last_name}  name ="last_name" onChange={(e)=>handleChange(e)} />
                           <span>{errors.last_name && (<p className='error'>{errors.last_name}</p>)}</span>
 				    </div>
-                   
+
+
+
+
+
                     <div className="space-y-1 mb-2">
 					    <label className="block mb-1 text-sm ml-1">Nombre de usuario</label>
 				        <input type="text" className="w-full px-3 py-2 border rounded-md border-gray-700 bg-gray-900
@@ -134,7 +138,9 @@ export default function Registrar(){
 				    </div>
                 </div>
 
+
                 <div className='flex flex-col'>
+
 
                     <div className="space-y-1 mb-4">
 				            <label className="block mb-1 text-sm ml-1">Correo electrónico</label>
@@ -143,6 +149,8 @@ export default function Registrar(){
                             />
                              <span>{errors.email && (<p className='error'>{errors.email}</p>)}</span>
 			        </div>
+
+
                 
                     <div className="space-y-1">
 					    <label className="block mb-1 text-sm ml-1">Contraseña</label>
@@ -151,9 +159,10 @@ export default function Registrar(){
                          />
                           <span>{errors.password && (<p className='error'>{errors.password}</p>)}</span>
 				    </div>
-                        	
+
                     </div>
                     </div>
+
 
 		            <div className="space-y-1">
 			            <div>
@@ -162,15 +171,19 @@ export default function Registrar(){
                                 Registrarse
                             </button>
 			            </div>
-
-                       
 		            </div>
+
+
 	            </form>
-                <p className="px-2 mr-1 text-sm text-center text-gray-400">¿Ya tienes una cuenta?
-                            <Link to='/login'>
-				                <a rel="noopener noreferrer" href="#" className="hover:underline text-indigo-400"> Inicia sesión</a>.
-                            </Link>
-			            </p>
+
+                <div>
+                    <p className="px-2 mr-1 text-sm text-center text-gray-400">¿Ya tienes una cuenta?
+                        <Link to='/login'>
+				            <a rel="noopener noreferrer" href="#" className="hover:underline text-indigo-400"> Inicia sesión</a>.
+                        </Link>
+			        </p>
+                </div>
+              
             </div>
 
             <div className="container-footer mt-auto">
